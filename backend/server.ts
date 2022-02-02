c7f4be76d7ec41c662c1f8dbb17dea0c336b75d3
Tomás Catena <tomascatena@gmail.com>
2022-02-02 21:09:25 +0100

    initial project setup

---
+++ backend/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening in port ${PORT}`);
+});
