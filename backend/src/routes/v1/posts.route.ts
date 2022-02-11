22beef6030f5b1e31694395eca66c35532ba3e4a
Tomás Catena <tomascatena@gmail.com>
2022-02-11 19:41:31 +0100

    complete like post functionality, update error handling

---
+++ backend/src/routes/v1/posts.route.ts
@@ -0,0 +1,67 @@
+import { postsControllers } from '@controllers/index';
+import { postsValidation } from '@middleware/validations';
+import { requireAuth } from '@middleware/requireAuth.middleware';
+import { Router } from 'express';
+
+const router = Router();
+
+// @route     POST api/v1/posts
+// @desc      Create a post
+// @access    Private
+router.post(
+  '/',
+  postsValidation.createPost,
+  requireAuth,
+  postsControllers.createPost
+);
+
+// @route     GET api/v1/posts/:postId
+// @desc      Get post by id
+// @access    Private
+router.get(
+  '/:postId',
+  postsValidation.getPostById,
+  requireAuth,
+  postsControllers.getPostById
+);
+
+// @route     GET api/v1/posts
+// @desc      Get all posts
+// @access    Private
+router.get(
+  '/', //
+  requireAuth,
+  postsControllers.getAllPosts
+);
+
+// @route     DELETE api/v1/posts/:postId
+// @desc      Delete post by id
+// @access    Private
+router.delete(
+  '/:postId',
+  postsValidation.deletePostById,
+  requireAuth,
+  postsControllers.deletePostById
+);
+
+// @route     PUT api/v1/posts/like/:postId
+// @desc      Like post
+// @access    Private
+router.put(
+  '/like/:postId',
+  postsValidation.likePost,
+  requireAuth,
+  postsControllers.likePost
+);
+
+// @route     PUT api/v1/posts/unlike/:postId
+// @desc      Like post
+// @access    Private
+router.put(
+  '/like/:postId',
+  postsValidation.likePost,
+  requireAuth,
+  postsControllers.likePost
+);
+
+export default router;
