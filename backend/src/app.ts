a382fb8dee2b379e91ed33a4ce13a078581d45eb
Tomás Catena <tomascatena@gmail.com>
2022-02-12 22:00:04 +0100

    add express-rate-limit and hpp middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,36 +9,51 @@ import cors from 'cors';
 import { notFound } from '@middleware/notFound.middleware';
 // @ts-ignore
 import xssClean from 'xss-clean';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
 import { expressWinstonLogger } from './config/logger';
 
 export const app = express();
 
-// secure Express app by setting various HTTP headers
+// Secure Express app by setting various HTTP headers
 app.use(helmet());
 
 // Prevent cross site scripting attacks
 app.use(xssClean());
+
+// Sanitize request data
+app.use(mongoSanitize());
+
+// Basic rate-limiting middleware for Express
+app.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 Minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 
 if (env.NODE_ENV === 'development') {
   // HTTP request logger middleware
   app.use(morganHttpLogger);
 }
 
-// parse json request body
+// Parse json request body
 app.use(express.json({ limit: '1kb' }));
 
-// parse urlencoded request body
+// Parse urlencoded request body
 app.use(express.urlencoded({ extended: true, limit: '1kb' }));
 
-// sanitize request data
-app.use(mongoSanitize());
+// Express middleware to protect against HTTP Parameter Pollution attacks
+app.use(hpp());
 
-// enable cors
+// Enable cors
 app.use(cors());
-// enable pre-flight
+// Enable pre-flight
 app.options('*', cors);
 
-// express-winston logger makes sense BEFORE the router
+// Express-winston logger makes sense BEFORE the router
 app.use(expressWinstonLogger.info);
 
 // v1 api Documentation
@@ -47,13 +62,13 @@ app.use('/api/v1/docs', express.static('public'));
 // v1 api routes
 app.use('/api/v1', routes);
 
-// express-winston errorLogger makes sense AFTER the router
+// Express-winston errorLogger makes sense AFTER the router
 app.use(expressWinstonLogger.error);
 
 // Fallback for not found requests
 app.use(notFound);
 
-// convert error to ApiError, if needed
+// Convert error to ApiError, if needed
 app.use(errorConverter);
 
 // Error handler for failed requests
